8fa806b96a18bf7940ac068dcf72f72f26d7df6d
sravan-addicherla <sravanaddicherla@gmail.com>
2021-12-12 03:23:44 +0530

    done

--- src/components/Calsi.js
+++ src/components/Calsi.js
@@ -1,115 +1,78 @@
 import React,{useState,useEffect} from 'react';
+import * as math from 'mathjs';
 
 function Calsi() {
     const [data,setData]=useState("");
     const [input,setInput]=useState("");
-    const [curState,setCurState]=useState("");
-    const [prevState,setPrevState]=useState("");
-    const [operator,setOpeartor]=useState(null);
-
+    const [operator,setOpeartor]=useState(true);
+    
     const handleChange=(e)=>{
-        if(curState.includes(".") && e.target.name===".") return ;
-
-        curState===""
-        ?setCurState(e.target.name)
-        :setCurState(prev=>prev+e.target.name);
+        if(e.target.className==="operators" && operator===false && data!=="") return;
+        if(e.target.className==="operators"){
+            setOpeartor(false);
+        }else{
+            setOpeartor(true);
+        }
+        setInput(prev=>prev+e.target.name);
     }
 
-    useEffect(()=>{
-        setInput(curState);
-    },[curState]);
 
     useEffect(() => {
-        setInput("0");
-      }, []);
+        if(operator===false)return;
+        if(isNaN(Number(input.charAt(input.length-1)))){
+            setData(math.evaluate(input.substring(0,input.length-1)));
+        }else{
+            setData(math.evaluate(input))
+        }
+      }, [input,operator]);
 
 
     const deleteIt=()=>{
-        setCurState(input.substring(0,input.length-1));
+        setInput(input.substring(0,input.length-1));
     }
 
     const clearIt=()=>{
-        setInput("0");
-        setCurState("");
-        setPrevState("");
+        setInput("");
         setData("");
     }
 
     const signChange=()=>{
-        if(curState.charAt(0)==="-"){
-            setCurState(curState.substring(1));
-        }else{
-            setCurState("-"+curState);
-        }
+       
     }
 
-    const addOperator=(e)=>{
-        setOpeartor(e.target.name);
-        if (curState === "") return;
-        if (prevState !== "") {
-            setData(prev=>prev+curState+e.target.name);
-            result();
-        } else {
-            setData(prev=>prev+curState+e.target.name);
-            setPrevState(curState);
-            setCurState("");
-        }
+    const result=()=>{
+        setInput(data);
+        setOpeartor(false);
+        setData("");
     }
-
-
-    const result=(e)=>{
-        if(prevState==="" || curState==="") return ;
-        if(e?.target.name==="="){
-            setData(prev=>prev+curState);
-        }
-
-        let total;
-        switch (operator) {
-            case "/":
-              total = String(parseFloat(prevState) / parseFloat(curState));
-              break;
-      
-            case "+":
-              total = String(parseFloat(prevState) + parseFloat(curState));
-              break;
-            case "x":
-              total = String(parseFloat(prevState) * parseFloat(curState));
-              break;
-            case "-":
-              total = String(parseFloat(prevState) - parseFloat(curState));
-              break;
-            default:
-              return;
-        }
-        setCurState("");
-        setPrevState(total);
-        setInput("");
-    }
-
+    
+    
     return (
         <div className="work-box">
-            <input type="text" id="data" value={data}/>
-            {input !== "" || input === "0"
-                ?<input type="text" value={input}/>
-                :<input type="text" value={prevState} />
+            <input type="text" id="data" value={input}/>
+            {
+                input===""
+                ?<input type="text" id="data" value={input}/>
+                : <input type="text" value={data} />
+                
             }
             <button className="operators" id="clearClass" onClick={clearIt}>AC</button>
             <button className="operators" id="delId" onClick={deleteIt}>C</button>
             <button name="+/-" className="operators" onClick={signChange}>+/-</button>
-            <button name="/" className="operators" onClick={addOperator}>/</button>
+            <button name="/" className="operators" onClick={handleChange}>/</button>
             <button name="7"onClick={handleChange}>7</button>
             <button name="8" onClick={handleChange}>8</button>
             <button name="9" onClick={handleChange}>9</button>
-            <button name="x" className="operators" onClick={addOperator}>x</button>
+            <button name="*" className="operators" onClick={handleChange}>x</button>
             <button name="4" onClick={handleChange}>4</button>
             <button name="5" onClick={handleChange}>5</button>
             <button name="6" onClick={handleChange}>6</button>
-            <button name="-" className="operators" onClick={addOperator}>-</button>
+            <button name="-" className="operators" onClick={handleChange}>-</button>
             <button name="1" onClick={handleChange}>1</button>
             <button name="2" onClick={handleChange}>2</button>
             <button name="3" onClick={handleChange}>3</button>
-            <button name="+" className="operators" onClick={addOperator}>+</button>
-            <button name="." onClick={handleChange}>.</button>
+            <button name="+" className="operators" onClick={handleChange}>+</button>
+            <button name="." className="operators" onClick={handleChange}>.</button>
             <button name="0" onClick={handleChange}>0</button>
             <button name="=" className="equals" onClick={result}>=</button>
         </div>
